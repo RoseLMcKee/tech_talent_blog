35db6a38c81478a09ec8429c7cd9e73cb2d64262
Rose McKee <rlnashmckee@gmail.com>
2019-10-10 09:48:13 -0500

    Comment scaffold created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_09_141807) do
+ActiveRecord::Schema.define(version: 2019_10_10_135139) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "comment_entry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
